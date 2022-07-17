ad7b614f6a3983d07fca24484b2f25a37f4ba406
Asif <asif.ca43@gmail.com>
2022-07-17 12:57:33 +0500

    added custom 404 page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home'
 import AboutUs from './pages/About'
 import Courses from './pages/Courses'
 import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+import Page404 from './pages/Page404'
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
             <Route path="/" index  element={<Home/>} />
             <Route path="/courses"  element={<Courses/>} />
             <Route path="/about"  element={<AboutUs/>} />
+            <Route path="*" element={<Page404 />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/pages/Page404.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/page404.css'
+
+function Page404() {
+  return (
+      <div className="page404">
+        <div>
+          <h2>404</h2>
+          <h3 style={{marginTop : ""}}>Page Not Found</h3>
+        </div>
+      </div>
+  )
+}
+
+export default Page404
